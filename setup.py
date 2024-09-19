a3142bdefa9b1f309d5fabb32a5381193e9aa395
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2024-09-19 01:08:30 -0700

    Fix issues with the setup file Change example.py and README.md to import the Engine class from the tic_tac_toe_engine tic_tac_toe_engine package instead of directly importing it from engine.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+
+VERSION = '0.1.0'
+DESCRIPTION = 'An implementation of the Tic Tac Toe game rules'
+LONG_DESCRIPTION = 'An engine that provides full implementation of the Tic Tac Toe Game'
+
+# Setting up
+setup(
+	name="tic-tac-toe-engine",
+	version=VERSION,
+	author="Sebastian Mendoza",
+	author_email="<sebastian.mendoza.clases@gmail.com>",
+	description=DESCRIPTION,
+	long_description_content_type="text/markdown",
+	long_description=LONG_DESCRIPTION,
+	packages=find_packages(),
+	install_requires=['typing', 'random'],
+	keywords=['python', 'tic-tac-toe', 'tictactoe', 'game', 'engine'],
+	classifiers=[
+		"Development Status :: 2 - Pre-Alpha",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python :: 3",
+		"Operating System :: Unix",
+		"Operating System :: MacOS :: MacOS X",
+		"Operating System :: Microsoft :: Windows",
+	]
+)
